ca339f808039db9b22cd970fd4f69c11e03dc810
Ekaterina Krutakova <lagoonnw@gmail.com>
2019-07-02 16:31:16 +0500

    css

--- gulpfile.js
+++ gulpfile.js
@@ -31,6 +31,7 @@ gulp.task("style", function(cb) {
     .pipe(gulp.dest("build/css"))
     .pipe(minify())
     .pipe(rename("style.min.css"))
+    .pipe(gulp.dest("css"))
     .pipe(gulp.dest("build/css"))
     .pipe(server.stream());
   cb();
